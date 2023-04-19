d55cb8cbc44dc2dc487dcba040b0986786a50fb9
soumyadevporiya <75175280+soumyadevporiya@users.noreply.github.com>
2023-04-19 12:19:35 +0530

    let10 copies of micro-services read 10 partitions

--- create_pod_v4.py
+++ create_pod_v4.py
@@ -18,19 +18,19 @@ from kubernetes.client import V1EnvVar
 config.load_incluster_config()
 v1 = kubernetes.client.CoreV1Api()
 
-for i in range(0, 7):
+for i in range(0, 10):
     container_name = 'container-with-envs-{}'.format(i)
     namespace = 'default'
     pod_name = 'my-bq-read-pod-{}'.format(i)
 
-    image = 'gcr.io/level-approach-382012/ns_monolith_parallel_reader_v2:latest'
+    image = 'gcr.io/level-approach-382012/ns_monolith_parallel_reader_v4:latest'
     env1 = V1EnvVar(name='ITER', value=str(i))
     env2 = V1EnvVar(name='POD_TYPE', value='1st')
-    env3 = V1EnvVar(name='PCT', value='0')
+    env3 = V1EnvVar(name='PCT', value='1')
     container = V1Container(name=container_name, image=image, env=[env1, env2, env3])
 
     node_name = ''
-    if i <= 4:
+    if i <= 5:
         node_name = 'gke-cluster-1-default-pool-9962d16b-plm6'
     else:
         node_name = 'gke-cluster-1-pool-1-5117341c-zkx8'
