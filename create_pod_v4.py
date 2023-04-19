8d9428f771749807cd91e2ca5ae35df847f316e5
soumyadevporiya <75175280+soumyadevporiya@users.noreply.github.com>
2023-04-19 13:34:14 +0530

    let10 copies of micro-services read 10 partitions

--- create_pod_v4.py
+++ create_pod_v4.py
@@ -30,9 +30,9 @@ for i in range(0, 10):
     container = V1Container(name=container_name, image=image, env=[env1, env2, env3])
 
     node_name = ''
-    if i <= 4:
+    if i <= 3:
         node_name = 'gke-cluster-1-default-pool-b22acbbd-ljjc'
-    elif i <= 7:
+    elif i <= 6:
         node_name = 'gke-cluster-1-default-pool-b22acbbd-mhk6'
     else:
         node_name = 'gke-cluster-1-default-pool-b22acbbd-znfv'
